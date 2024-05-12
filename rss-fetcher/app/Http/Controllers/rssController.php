d58b36637757490f1326b2d07d00580181137867
INTEL-ONBOARD <75471327+INTEL-ONBOARD@users.noreply.github.com>
2024-05-12 23:48:47 +0530

    fixes
    little fixes

--- rss-fetcher/app/Http/Controllers/rssController.php
+++ rss-fetcher/app/Http/Controllers/rssController.php
@@ -21,89 +21,61 @@ class rssController extends Controller
         $feed->enable_cache(false);
         $feed->set_output_encoding('utf-8');
 
-        if ($feed->init()) {
+        if (!$feed->init()) {
+                try{
+                    $opts = array('http' => array('header' => "User-Agent:MyAgent/1.0\r\n")); 
+                    $context = stream_context_create($opts);
+                    $response = file_get_contents($request->url, false, $context);
+                    $feed = new SimplePie();
+                    $feed->set_raw_data($response);
+                    $feed->init();
+                }catch(\Exception $e){
+                    return redirect()->route('get.home')->with('error','Error fetching XML content. Please try again later. '.$e->getMessage());
+                }
+
+        }
+        try{
             foreach ($feed->get_items() as $item) {
-                rss_job::create([
+                $params=[
                     'title'=>(string)$item->get_title() ,
                     'date'=> (string)$item->get_date() ,
                     'link'=> (string)$item->get_link(),
-                    'author'=> (string)$item->get_author(),
                     'description'=> (string)$item->get_description(),
-                    'category'=> (string)$item->get_category(),
                     'guid'=> (string)$item->get_id(),
-                ]);
-            }
-            return redirect()->route('get.home')->with('success', 'RSS feed fetched and stored successfully.');
-        } else {
-
-            try {
-                $opts = array('http' => array('header' => "User-Agent:MyAgent/1.0\r\n")); 
-                $context = stream_context_create($opts);
-                $response = file_get_contents($request->url, false, $context);
-            
-                if ($response !== false) {
-
-                    error_log($response);
-            
-
-                    $xml = @simplexml_load_string($response);
-            
-                    if ($xml !== false) {
-                        foreach ($xml->channel->item as $item) {
-                            rss_job::create([
-                                'title'=> $item->get_title ,
-                                'date'=> (string)$item->get_date,
-                                'link'=> (string)$item->get_link,
-                                'author'=> (string)$item->get_author,
-                                'description'=> (string)$item->get_description,
-                                'category'=> (string)$item->get_category,
-                                'guid'=>(strlen((string)$item->get_id) > 1500) ? null : (string)$item->get_id,
-                            ]);
-                        }
-                        return redirect()->route('get.home')->with('success', 'RSS feed fetched and stored successfully.');
-                    } else {
-                        // Failed to parse XML content
-                        return redirect()->route('get.home')->with('error', 'Error parsing XML content. Please try again later.');
-                    }
-                } else {
-                    // Failed to fetch XML content
-                    return redirect()->route('get.home')->with('error', 'Error fetching XML content. Please try again later.');
-                }
-            } catch (\Exception $e) {
-
-                return redirect()->route('get.home')->with('error', $e->getMessage());
+                ];
+                if ($item->get_author() ? $params['author'] = (string)$item->get_author()->get_name():$params['author']= null);
+                if ($item->get_category() ? $params['category'] = (string)$item->get_category()->get_label():$params['category']= null);
+                rss_job::create($params);
             }
             
-
-            // // try{
-
-            //     $opts = array('http'=>array('header' => "User-Agent:MyAgent/1.0\r\n")); 
-            //     $context = stream_context_create($opts);
-            //     $response = file_get_contents($request->url, false, $context);
-            //     if ($response != false) {
-            //         // $response = file_get_contents($request->url);
-            //         $xml = simplexml_load_string($response);
-            //         // dd($xml);
-            //         foreach ($xml->channel->item as $item) {
-            //             rss_job::create([
-            //                 'title'=> $item->get_title ,
-            //                 'date'=> (string)$item->get_date,
-            //                 'link'=> (string)$item->get_link,
-            //                 'author'=> (string)$item->get_author,
-            //                 'description'=> (string)$item->get_description,
-            //                 'category'=> (string)$item->get_category,
-            //                 'guid'=>(strlen((string)$item->get_id) > 1500) ? null : (string)$item->get_id,
-            //             ]);
-            //         }
-            //         return redirect()->route('get.home')->with('success', 'RSS feed fetched and stored successfully.');
-            //         // $feed->error()
-                    
-            //     }else{
-            //         return redirect()->route('get.home')->with('error', 'Error initializing feed. Please try again later.');
-            //     }
-            // // } catch (\Exception $e) {
-            // //     return redirect()->route('get.home')->with('error',$e->getMessage());
-            // // }
+            return redirect()->route('get.home')->with('success', 'RSS feed fetched and stored successfully.'. sizeof( $feed->get_items()).' items stored!');
+        }catch (\Exception $e){
+            return redirect()->route('get.home')->with('error','Error fetching XML content. Please try again later. '.$e->getMessage());
         }
+
     }
 }
+
+
+
+
+
+// if ($feed->init()) {
+//     foreach ($feed->get_items() as $item) {
+//         $params=[
+//             'title'=>(string)$item->get_title() ,
+//             'date'=> (string)$item->get_date() ,
+//             'link'=> (string)$item->get_link(),
+//             'description'=> (string)$item->get_description(),
+//             'guid'=> (string)$item->get_id(),
+//         ];
+//         if ($item->get_author() ? $params['author'] = (string)$item->get_author()->get_name():$params['author']= null);
+//         if ($item->get_category() ? $params['category'] = (string)$item->get_category()->get_label():$params['category']= null);
+//         rss_job::create($params);
+//     }
+//     return redirect()->route('get.home')->with('success', 'RSS feed fetched and stored successfully.');
+// } else {
+//     // Failed to fetch XML content
+//     return redirect()->route('get.home')->with('error', 'Error fetching XML content. Please try again later.');
+// }
+// // return redirect()->route('get.home')->with('error', $e->getMessage());
--- rss-fetcher/app/Models/rss_job.php
+++ rss-fetcher/app/Models/rss_job.php
@@ -24,24 +24,3 @@ class rss_job extends Model
 }
 
 
-
-        // echo "<p>date:". $item->get_date() . "</p><br>";
-        // echo "<p>Title: " . $item->get_title() . "</p>";
-        // echo "<p>link:". $item->get_link() . "</p>";
-        // echo "<p>Description:". $item->get_description() . "</p>";
-        // echo "<p>Category:". $item->get_category(). "</p>";
-        // echo "<p>guid:". $item->get_id(). "</p>";
-
-
-
-        // //$response = Http::get($rssUrl);
-        // $response = file_get_contents($rssUrl);
-        // dd($response);
-        // // $xml = simplexml_load_string($response);
-        // // //dd($xml);
-        // // foreach ($xml->channel->item as $item) {
-        // //     echo "<p>Title: " . (string) $item->title . "</p>";
-        // //     echo "<p>Link: " . (string) $item->link . "</p>";
-        // //     echo "<p>Description: " . (string) $item->description . "</p>";
-        // //     echo "<br>";
-        // // }
